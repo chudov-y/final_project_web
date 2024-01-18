94006542d7a1b8840611ae10b38012edf44771ca
sound <chaki.bubalo@gmail.com>
2024-01-18 23:21:35 +0500

    edited PagesLinks.java

--- src/test/java/pages/PagesLinks.java
+++ src/test/java/pages/PagesLinks.java
@@ -4,23 +4,17 @@ public enum PagesLinks {
 
     REGISTRATION_FORM("/automation-practice-form", "Practice Form"),
     LOGIN("/login", "Login"),
-   // BOOK_STORE("/books", "Book Store"),
     TEXT_BOX("/text-box", "Text Box"),
     PROFILE("/profile", "Profile");
 
     private final String link;
-   //         header;
 
     PagesLinks(String link, String header) {
         this.link = link;
-    //    this.header = header;
     }
 
     public String getLink() {
         return link;
     }
 
-//    public String getHeader() {
-//        return header;
-//    }
 }
